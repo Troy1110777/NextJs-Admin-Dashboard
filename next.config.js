7bc9a2fce095fa9edb3566882688481ebe5eb470
Prarinam Deria <93766302+prarinam@users.noreply.github.com>
2023-08-27 21:22:53 +0530

    .gitignore modified

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+// import dotenv from 'dotenv'
+// dotenv.config()
 const nextConfig = {
-    env:{
-        DB_URI_DEV:"mongodb://localhost:27017/NextJs_MongoDB_Dashboard",
-        GoogleProvider_Client_Id:'615095495389-hoi6pbsialp3ps439h2vbln0kikfqemq.apps.googleusercontent.com',
-        GoogleProvider_Client_Secret:'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'
+    env: {
+        DB_URI_DEV: "mongodb://localhost:27017/NextJs_MongoDB_Dashboard",
+        GoogleProvider_Client_Id: '615095495389-hoi6pbsialp3ps439h2vbln0kikfqemq.apps.googleusercontent.com',
+        GoogleProvider_Client_Secret: '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'
     }
 }
 
